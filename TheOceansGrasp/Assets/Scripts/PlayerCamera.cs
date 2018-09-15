bd2656374f340c7dc46fcb2a3bd030b280cf40d9
Matthew Lefurge (RIT Student) <msl5994@ad.rit.edu>
2018-09-15 16:42:24 -0400

    More Camera Code

--- TheOceansGrasp/Assets/Scripts/PlayerCamera.cs
+++ TheOceansGrasp/Assets/Scripts/PlayerCamera.cs
@@ -1,30 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.InteropServices;
 
 public class PlayerCamera : MonoBehaviour {
-
     float sensitivity = 0.05f;
     Vector3 playerCamForward = new Vector3(0.0f,0.0f,0.0f);
     public Camera playerCamera;
+    float screenCenterX;
+    float screenCenterY;
     float mouseX;
     float mouseY;
+    float distanceX;
+    float distanceY;
+
+    //[DllImport("user32.dll")]
+    //static extern bool SetCursorPos(float X, float Y);
 
     // Use this for initialization
     void Start ()
     {
+        // get the center of the screen
+        screenCenterX = Camera.current.pixelWidth / 2.0f;
+        screenCenterY = Camera.current.pixelHeight / 2.0f;
+
+        //screenCenter = new Vector2(Screen.width / 2.0f, Screen.height / 2.0f);
         //playerCamera = GetComponent<Camera>();
         //transform.LookAt(playerCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, playerCamera.nearClipPlane)), Vector3.up);
-	}
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        //mouseX = Input.mousePosition.x;
-        //mouseY = Input.mousePosition.y;
+        // get the mouse positions
+        mouseX = Input.mousePosition.x;
+        mouseY = Input.mousePosition.y;
+
+        // calculate the distance this frame
+        distanceX = mouseX - screenCenterX;
+        distanceY = mouseY - screenCenterY;
 
         // get the forward vector of the camera
-        playerCamForward = playerCamera.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, playerCamera.nearClipPlane));
+        playerCamForward = playerCamera.ScreenToViewportPoint(new Vector3(distanceX, distanceY, playerCamera.nearClipPlane));
 
         // adjust the rotational speed based on the sensistivity value
         playerCamForward.x -= 0.5f;
@@ -41,6 +58,11 @@ public class PlayerCamera : MonoBehaviour {
         Vector3 worldPoint = playerCamera.ScreenToWorldPoint(screenPoint);
 
         // have the camera look at the world point
-        transform.LookAt(worldPoint, Vector3.up);       
+        playerCamera.transform.LookAt(worldPoint, Vector3.up);
+        //transform.LookAt(worldPoint, Vector3.up);
+
+        // reset the mouse position to the center of the screen
+        Input.mousePosition.Set(screenCenterX,screenCenterY,0.0f);
+        //SetCursorPos(mouseX, mouseY);
     }
 }
